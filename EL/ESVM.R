e809444519f870f01cd7f6fa3a8d758537031f29
TieNan2019 <cooldownlengque@163.com>
2020-06-06 18:16:33 +0800

    支持向量机进行集成

---
+++ EL/ESVM.R
@@ -0,0 +1,137 @@
+#! /usr/bin/env Rscript
+
+
+library(tidyverse)
+library(caret)
+library(caretEnsemble)
+
+
+fold_numbers = 10
+val_times = 3
+
+
+dataset <- read.csv("dataset.csv")
+
+train_control <- trainControl(
+        method = "repeatedcv", 
+        number = fold_numbers, 
+        repeats = val_times,
+        savePredictions = TRUE,
+        classProbs = TRUE
+)
+
+train_index <- createDataPartition(
+        dataset$健康,
+        p = .75,
+        list = FALSE,
+        times = 1,
+)
+# train_index
+# c(1, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+# 17, 19, 20, 22, 23, 24, 25, 26, 27, 30, 32, 33, 34, 
+# 35, 36, 38, 40, 41, 42, 43, 46, 47, 48, 49, 52, 53, 
+# 55, 58, 59, 61, 62, 63, 64, 66, 67, 69, 70, 71, 72, 
+# 74, 75, 76, 77, 79, 80, 82, 83, 84, 85, 86, 89, 90, 
+# 91, 93, 94, 95, 96, 97, 98, 99, 100, 102, 104, 105, 
+# 107, 108, 109, 111, 113, 115, 116, 117, 118, 119, 
+# 120, 121, 122, 124, 127, 128, 129, 132, 133, 134, 
+# 135, 136, 138, 140, 141, 142, 143, 144, 145, 146, 
+# 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 
+# 158, 161, 164, 165, 166, 169, 170, 171, 172, 173, 
+# 174, 176, 177, 179, 180, 181, 182, 183, 186, 188, 
+# 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 
+# 200, 201, 202, 203, 204, 205, 206, 207, 208, 210, 
+# 211, 212, 213, 216, 219, 220, 221, 222, 223, 225, 
+# 227, 228, 229, 230, 231, 232, 233, 236, 237, 238, 
+# 239, 240, 242, 243, 244, 248, 249, 250, 252, 253, 
+# 254, 257, 258, 259, 260, 261, 262, 263, 264, 265)
+
+train_data <- dataset[train_index, ]
+test_data <- dataset[-train_index, ]
+
+
+# 数据集采样方式
+train_control <- trainControl(
+        # 使用 10 折交叉检验
+        method = "repeatedcv",
+        number = fold_numbers,
+        # 重复次数
+        repeats = val_times,
+        savePredictions = 'final',
+        classProbs = TRUE#,
+        # index = createFolds(train_data$健康, fold_numbers)
+)
+
+# 网格搜索
+# 超参数网格
+params_grid <- expand.grid(sigma = 2^(-10:4), C = 1:20)
+
+# 搜索最佳参数
+# svm_model <- train(
+#         # 指定标签和特征
+#         健康 ~ .,
+#         # 训练集
+#         data = train_data,
+#         # 选择算法
+#         method = "svmRadial",
+#         # 数据预处理
+#         preProc = c("center", "scale", "pca"),
+#         trControl = train_control,
+#         # 显示过程内容
+#         verbose = TRUE,
+#         # 自定义调参网格
+#         tuneGrid = params_grid
+# )
+
+# predicted <- predict(
+#         svm_model,
+#         newdata = test_data
+# )
+# Metrics::ce(predicted, test_data$健康)
+
+# svm_model$bestTune
+#    sigma       C
+# 40 0.001953125 20
+
+
+model_list <- c(
+        # 线性核支持向量机
+        # 'svmLinear',
+        # 线性核 L2 正则化支持向量机 ———— 无法用于集成
+        # 'svmLinear3',
+        # 最小平方支持向量机 ———— 无法用于集成
+        # 'lssvmLinear'
+        # 多项式核最小平方支持向量机 ———— 无法用于集成
+        # 'lssvmPoly',
+        # 指数核支持向量机 ———— 会报错
+        # 'svmExpoString',
+        # 高斯核最小平方支持向量机 ———— 无法用于集成
+        # 'lssvmRadial',
+        # 高斯核偏置支持向量机
+        # 'svmRadial'
+)
+
+# 分别生成模型
+models <- caretList(
+        健康 ~ .,
+        data = train_data,
+        trControl = train_control,
+        methodList = model_list
+        # tuneList = list(
+        #         svmRadial = caretModelSpec(
+        #                 method="svmRadial", 
+        #                 trace = FALSE, 
+        #                 tuneGrid = params_grid
+        #         )
+        # )
+)
+
+
+# 模型集成
+svm_stack <- caretStack(
+        models, 
+        method = "rf", 
+        metric = "Accuracy", 
+        trControl = train_control
+)
+
